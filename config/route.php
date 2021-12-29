3fc87bf4b67359964ee3407d5e106fec0260076a
maoxp <maoxingpei8686@163.com>
2021-12-29 16:42:48 +0800

    fixed

--- config/route.php
+++ config/route.php
@@ -12,6 +12,7 @@
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
+use app\controller\Index;
 use Webman\Route;
 
 
@@ -19,3 +20,18 @@ use Webman\Route;
 
 
 
+// 关闭默认路由规格
+Route::disableDefaultRoute();
+
+// 回退路由: 处理不存在的路由And更改默认404
+Route::fallback(static function () {
+    return response('not null');
+});
+
+Route::options('[{path:.+}]', static function (){
+    return response('');
+});
+
+Route::group('/api/index', static function () {
+    Route::post( '/json', [Index::class, 'json']);
+});
